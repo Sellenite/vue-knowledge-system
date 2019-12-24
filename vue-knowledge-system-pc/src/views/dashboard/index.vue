7b9773c9aa14a9efad1b21dc12ae18eb900c1760
周健明 <714627528@qq.com>
2019-12-24 18:20:35 +0800

    init

---
+++ vue-knowledge-system-pc/src/views/dashboard/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dashboard-container">
+    123
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+
+  },
+  created() {
+
+  }
+}
+</script>
